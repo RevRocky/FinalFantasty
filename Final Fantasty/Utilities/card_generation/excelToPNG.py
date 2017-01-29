8a3558e70bf6426f52c2783e0c0fc6ce62b3d804
Rokcky Petkov <rocky@weeklybeat.org>
2017-01-29 11:04:07 -0500

    Writing text on card images
    Font Rescaling now works and titles are able to be written to cards. I
    have also included the first iteration of a font I'd like to use on the
    cards, opting to use the Bitter Typeface

--- Final Fantasty/Utilities/card_generation/excelToPNG.py
+++ Final Fantasty/Utilities/card_generation/excelToPNG.py
@@ -7,8 +7,9 @@ Version 0.1
 Author: Rocky Petkov"""
 
 
-from PIL import ImageDraw, Image
+from PIL import ImageDraw, Image, ImageFont
 import os
+import copy
 
 
 # Below are some global constants regarding different colours, fonts and file locations to be used with this module.
@@ -17,9 +18,17 @@ import os
 CARD_ART_DIRECTORY = ".." + os.sep + ".." + os.sep + "Art"                 # Path to Card Art!
 CARD_DIRECTORY = ".." + os.sep + ".." + os.sep + "Art" + os.sep + "Cards"  # Path to Final Card Images
 
+CARD_FONT = ImageFont.truetype("Bitter-Regular.otf", size=28)
+CARD_FONT_BOLD = ImageFont.truetype("Bitter-Bold.otf", size=50)
+CARD_FONT_ITALIC = ImageFont.truetype("Bitter-Italic.otf", size=28)
+
+BLACK = (0, 0, 0)
+
+
 PICTURE_TOP_LEFT = (80, 77)                                                # Top left corner of the picture area
 PICTURE_SIZE = (576, 577)                                                  # Size of picture area
-NAME_BOUNDARIES = ((0, 0), (0, 0), (0, 0), (0, 0))                         # Boundaries of card name
+NAME_LOCATION = (368, 701)                                                 # Boundaries of card name
+NAME_BOUNDARIES = (515, 70)                                                # Size of card area alotted to name
 TYPE_BOUNDARIES = ((0, 0), (0, 0), (0, 0), (0, 0))                         # Boundaries of card type descriptors
 DESCRIPTION_BOUNDARIES = ((0, 0), (0, 0), (0, 0), (0, 0))                  # Boundaries of Mechanic Description
 PICTOGRAMME_BOUNDARIES = ((0, 0), (0, 0), (0, 0), (0, 0))                  # Boundaries of where the stats are
@@ -37,7 +46,16 @@ def construct_card_image(card_description):
     card_art = Image.open(CARD_ART_DIRECTORY + os.sep + card_description["Type"] + os.sep +
                           card_description["Art"], 'r')
     card_image = overlay_image(card_template, card_art, PICTURE_TOP_LEFT)    # Overlay the card art on to template
-    card_image.save(card_description["Picture Location"] + ".png", "PNG")
+
+    # Now we overlay the title on the image
+    image_draw = ImageDraw.Draw(card_image)
+    resized_font, width, null = fit_text(NAME_BOUNDARIES, CARD_FONT_BOLD, card_description["Name"], image_draw)
+    centered_top_left = (NAME_LOCATION[0] - (width/2), NAME_LOCATION[1])
+
+    image_draw.multiline_text(xy=centered_top_left, text=card_description["Name"].upper(), font=resized_font,
+                                fill=BLACK, align="left")
+    card_image.show()
+
 
 
 
@@ -45,12 +63,34 @@ def construct_card_image(card_description):
 The foreground image must be smaller than the background image Location in this case is an (X, Y) tuple
 describing the location ofv where the centre of the new image shall be"""
 def overlay_image(background_image, foreground_image, location):
-
     # Check to ensure the foreground image is of correct size. If it is not resize it.
     if (foreground_image.width != PICTURE_SIZE[0]) or (foreground_image.height != PICTURE_SIZE[1]):
         foreground_image = foreground_image.resize(PICTURE_SIZE)
-    background_image.paste(foreground_image, PICTURE_TOP_LEFT)
-    return background_image.convert(mode="RGBA")
+
+    # We must do two quick conversions to ensure that the paste will work correctly and tere will be no artefacts
+    # TODO put this into a conditional
+    background_image = background_image.convert(mode="RGB")             # Gettting rid of transparency on template
+    foreground_image = foreground_image.convert(mode="RGBA")            # Ensuring the image has an alpha channel
+
+    background_image.paste(foreground_image, PICTURE_TOP_LEFT, foreground_image)
+    return background_image
 
 
+"""Scales down a font until a message will fit in the desired area. The image_draw object must be supplied as well
+Returns:
+
+    font - a font object with an altered size. This is a modified deep copy of the original, so there will be no
+    unintended sideeffects
+    width, height - The width and height of the text block, used in aligning text"""
+def fit_text(area, font, message, image):
+    new_font = clone_font(font)                                 # Cloning the font so we can safely rescale
+    width, height = image.textsize(message.upper(), new_font)   # Calculating size of the message
+    while width > area[0] and height > area[1]:
+        new_font.size -= 5                                         # 5 Seems like a good thing to decrement by
+        width, height = image.textsize(message.upper(), new_font)  # Calculating size of font
+    return new_font, width, height
+
+"Returns a clone of the TrueType or OpenType font supplied"
+def clone_font(font):
+    return ImageFont.truetype(font.path, font.size)
 
