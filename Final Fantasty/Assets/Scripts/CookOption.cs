e57372b1a2d1882089a711c6b3dcf45ea8fd7977
Rokcky Petkov <rocky@weeklybeat.org>
2017-02-11 16:26:03 -0500

    Andrew's Work
    Credit where it is due

--- Final Fantasty/Assets/Scripts/CookOption.cs
+++ Final Fantasty/Assets/Scripts/CookOption.cs
@@ -3,11 +3,17 @@ using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+public class CookOption : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
 
 	public Text textBox;
 	public string message;
 	public IEnumerator messageRoutine;
+	public Button buttonBox;
+
+	// Use this for initialization
+	void Start () {
+		buttonBox.enabled = false;
+	}
 
 	public void OnPointerEnter(PointerEventData eventData) {
 		//Debug.Log("OnPointerEnter");
@@ -29,33 +35,34 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 		if(d != null && d.placeholderParent==this.transform) {
 			d.placeholderParent = d.parentToReturnTo;
 		}
+
+		if (transform.childCount >= 2) {
+			buttonBox.enabled = true;
+		} else {
+			buttonBox.enabled = false;
+		}
+
 	}
 
 	public void OnDrop(PointerEventData eventData) {
+
+
 		Debug.Log (eventData.pointerDrag.name + " was dropped on " + gameObject.name);
 
-		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
-		if(d != null) {
-			
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null) {
+
 			d.parentToReturnTo = this.transform;
-
-
 		}
 
-		if (textBox == null)
-			return;
-		
-		if (messageRoutine != null) {
-			StopCoroutine (messageRoutine);
-		}
-		messageRoutine = ShowMessage (message, 4);
-		StartCoroutine (messageRoutine);
-	}
 
-	IEnumerator ShowMessage(string text, float delay){
-		textBox.text = text;
-		textBox.enabled = true;
-		yield return new WaitForSeconds (delay);
-		textBox.enabled = false;
+		if (transform.childCount >= 2) {
+			buttonBox.enabled = true;
+		} else {
+			buttonBox.enabled = false;
+		}
+
+
+
 	}
 }
